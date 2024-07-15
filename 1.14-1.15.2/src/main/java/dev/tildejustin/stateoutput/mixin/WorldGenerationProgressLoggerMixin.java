6ae9687729fdce64feed1ff6a7b79dea3f435779
contaria <kingcontaria@gmail.com>
2024-07-15 18:57:02 +0200

    use wrapoperation for generation state capture (seedqueue compat)

--- 1.14-1.15.2/src/main/java/dev/tildejustin/stateoutput/mixin/WorldGenerationProgressLoggerMixin.java
+++ 1.14-1.15.2/src/main/java/dev/tildejustin/stateoutput/mixin/WorldGenerationProgressLoggerMixin.java
@@ -1,20 +1,25 @@
 package dev.tildejustin.stateoutput.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.tildejustin.stateoutput.StateOutputHelper;
 import net.minecraft.server.WorldGenerationProgressLogger;
 import net.minecraft.util.math.MathHelper;
-import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.apache.logging.log4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(WorldGenerationProgressLogger.class)
 public abstract class WorldGenerationProgressLoggerMixin {
     @Shadow
     public abstract int getProgressPercentage();
 
-    @Inject(method = "setChunkStatus", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;)V", shift = At.Shift.BEFORE))
-    private void outputGenerationState(CallbackInfo ci) {
-        // Using the getProgressPercentage to recalculate is slightly unoptimized but prevents needing to do locals capture, making it easier to port this mixin.
+    @WrapOperation(method = "setChunkStatus", at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;info(Ljava/lang/String;)V"))
+    private void outputGenerationState(Logger logger, String s, Operation<Void> original) {
+        // Using the getProgressPercentage to recalculate is slightly unoptimized but prevents needing to do locals capture, making it easier to port this mixin.#
+        // WrapOperation is used for compatibility with SeedQueue muting background loggers
         StateOutputHelper.updateLastState(state -> state.withProgress(MathHelper.clamp(getProgressPercentage(), 0, 100)));
+        original.call(logger, s);
     }
 }
