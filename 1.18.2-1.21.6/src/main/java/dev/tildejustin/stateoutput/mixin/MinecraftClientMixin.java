ce297d3ae1d20f6c0441dc47429fc693e6e6252f
tildejustin <spacepepper3.14@gmail.com>
2025-06-20 17:28:49 -0400

    1.2.2: fix mixin crash in 1.20.5-1.21.5

--- 1.18.2-1.21.6/src/main/java/dev/tildejustin/stateoutput/mixin/MinecraftClientMixin.java
+++ 1.18.2-1.21.6/src/main/java/dev/tildejustin/stateoutput/mixin/MinecraftClientMixin.java
@@ -20,7 +20,7 @@ public abstract class MinecraftClientMixin {
     public Screen currentScreen;
 
     @Dynamic
-    @Inject(method = {"disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", "method_56134(Lnet/minecraft/client/gui/screen/Screen;)V", "method_18096(Lnet/minecraft/client/gui/screen/Screen;Z)V"}, at = @At("TAIL"), require = 1, allow = 1)
+    @Inject(method = {"disconnect(Lnet/minecraft/client/gui/screen/Screen;)V", "method_18096(Lnet/minecraft/client/gui/screen/Screen;Z)V"}, at = @At("TAIL"), require = 1, allow = 1)
     private void outputWaitingState(CallbackInfo ci) {
         // We do this inject after this.player is set to null in the disconnect method.
         // This is because the inworld state output depends on the player being non-null,
